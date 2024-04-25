c1fd72d0be116ec4a28856d95bce89848f1f36fd
Austin <amcrussell03@gmail.com>
2024-04-25 15:15:20 -0400

    init fin

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,14 +1,20 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./views/Home";
 
 interface AppProps {}
 
 const App = (props: AppProps) => {
-
-	
-
-	return (
-		<p> austin russ</p>
-	);
+  return (
+    <div>
+      <BrowserRouter>
+        <div>{/* add navbar*/}</div>
+        <Routes>
+          <Route path="/" element={<Home></Home>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/client/views/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface homeProps {}
+
+const Home = (props: homeProps) => {
+  return (
+    <div className="container w-screen content-center pt-3">
+      <h1>Austin Russell</h1>
+    </div>
+  );
+};
+
+export default Home;
